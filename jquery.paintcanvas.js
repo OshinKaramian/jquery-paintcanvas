bb29effa9170dc204cdc944cfb73a605570faef9
Oshin Karamian <okaramia@gmail.com>
2011-04-17 22:44:12 -0700

    Cleaned up code and added an example file

--- jquery.paintcanvas.js
+++ jquery.paintcanvas.js
@@ -1,77 +1,84 @@
 (function( $ ){
-	$.fn.Paint = function(args) {
-		var draw= false;
-		var ctx = this[0].getContext("2d");
-		var currentPos = null;
-		var onMouseUp = function(){ };
+	$.fn.PaintCanvas = function(options) {
+		
+		this.settings = {
+			startPos : {x:-1,y:-1},
+			curPos : {x:-1,y:-1},
+			offsetPos : { x: 0, y: 0 },
+			onMouseMove : function() { },
+			onMouseDown: function() { },
+			onMouseUp: function() { },
+		}
+
 		var obj = this;
 
-		this.startPos = {x:-1,y:-1};
-		this.curPos = {x:-1,y:-1};
-		this.offsetPos = args.pos;
+		return this.each(function() {
 
-		var onMouseMove = args.onmove; 
-		var onMouseDown = args.ondown;
+			var draw = false;
+			var ctx = obj[0].getContext("2d");
 
-		ctx.strokeStyle = 'black';
+			ctx.strokeStyle = 'black';
 
-		//set it true on mousedown
-		this.mousedown(function(e){ 
-			draw=true; 
-			obj.startPos = getCanvasMousePos(e, obj.offsetPos);
-			onMouseDown(obj.startPos);
+			if ( options ) {
+				$.extend( obj.settings, options );
+			}
+
+			// -- Evented methods
+			obj.mousedown(function(e){ 
+				draw=true; 
+				obj.settings.startPos = getCanvasMousePos(e, obj.settings.offsetPos);
+				obj.settings.onMouseDown(obj.settings.startPos);
+			});
+
+			obj.mouseup(function(){
+				draw=false;
+				obj.settings.onMouseUp(obj.settings.curPos);
+			});
+
+			obj.mousemove(function(e) {
+				if(draw){
+					obj.settings.curPos = getCanvasMousePos(e, obj.settings.offsetPos);
+					obj.DrawOnCanvas(obj.settings.curPos);
+					obj.settings.onMouseMove(obj.settings.curPos);
+				}    
+			});
+
+			// -- Public Functions that are not based on events
+			obj.DrawOnCanvas = (function(currentPos){
+
+				ctx.save()
+				ctx.beginPath();
+				ctx.lineWidth = 5;
+				ctx.lineCap = "round";
+				ctx.moveTo(obj.settings.startPos.x, obj.settings.startPos.y);
+				ctx.lineTo(currentPos.x, currentPos.y);
+				ctx.stroke();
+				ctx.closePath();
+				ctx.restore();
+
+				obj.settings.startPos = currentPos;
+			}); 
+
+			obj.ClearScreen = (function(){
+				ctx.fillStyle = "#fff";
+				ctx.fillRect(0, 0, canvas.width, canvas.height);
+				ctx.strokeStyle = "black";
+				ctx.fillStyle = "black";
+			});
+
+			obj.SetColor = (function(color){
+				ctx.strokeStyle = color;
+			});
+
+			obj.SetEraser = (function(){
+				ctx.strokeStyle = obj.css("background-color");
+
+			});
+
+			// Private method to get mouse position
+			function getCanvasMousePos(e, position) {
+				return { x: e.clientX - position.x, y: e.clientY - position.y};
+			}			
 		});
-
-		//reset it on mouseup`
-		this.mouseup(function(){
-			draw=false;
-		});
-
-		this.mousemove(function(e) {
-			if(draw){
-				obj.curPos = getCanvasMousePos(e, obj.offsetPos);
-				obj.drawOnCanvas(obj.curPos);
-				onMouseMove(obj.curPos);
-			}    
-		});
-
-		this.drawOnCanvas = function(currentPos){
-
-			ctx.save()
-			ctx.beginPath();
-			ctx.lineWidth = 5;
-			ctx.lineCap = "round";
-			ctx.moveTo(obj.startPos.x, obj.startPos.y);
-			ctx.lineTo(currentPos.x, currentPos.y);
-			ctx.stroke();
-			ctx.closePath();
-			ctx.restore();
-
-			obj.startPos = currentPos;
-		};
-
-		function getCanvasMousePos(e, position) {
-			return { x: e.clientX - position.x, y: e.clientY - position.y};
-		}
-        
-       //code for color pallete
-		/*$("#clr > div").click(function(){
-			ctx.strokeStyle = $(this).css("background-color");
-		});*/
-     
-		//Eraser
-		$("#eraser").click(function(){
-			ctx.strokeStyle = '#fff';
-		});
-
-        //Clear 
-		$("#clear").click(function(){
-			ctx.fillStyle = "#fff";
-			ctx.fillRect(0, 0, canvas.width, canvas.height);
-			ctx.strokeStyle = "black";
-			ctx.fillStyle = "black";
-		});  
-		
-		return this;
   };
 })( jQuery );
